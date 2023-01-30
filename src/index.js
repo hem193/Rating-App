80be761b7928c8f3b477edc84c62b2090fc7d0cc
hem193 <hemanthcastel@gmail.com>
2023-01-30 15:17:55 +0530

    final-commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<h1>My App</h1>, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
